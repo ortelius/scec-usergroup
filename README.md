a6aba96486005d868c8163b54d492e9c626bd40a
sbtaylor15 <sbtaylor15@users.noreply.github.com>
2024-04-18 23:11:58 +0000

    update readme with latest swagger

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 > Version 11.0.0
 
 RestAPI for the Domain Object
+![Release](https://img.shields.io/github/v/release/ortelius/scec-domain?sort=semver)
+![license](https://img.shields.io/github/license/ortelius/scec-domain)
+
+![Build](https://img.shields.io/github/actions/workflow/status/ortelius/scec-domain/build-push-chart.yml)
+[![MegaLinter](https://github.com/ortelius/scec-domain/workflows/MegaLinter/badge.svg?branch=main)](https://github.com/ortelius/scec-domain/actions?query=workflow%3AMegaLinter+branch%3Amain)
+![CodeQL](https://github.com/ortelius/scec-domain/workflows/CodeQL/badge.svg)
+[![OpenSSF-Scorecard](https://api.securityscorecards.dev/projects/github.com/ortelius/scec-domain/badge)](https://api.securityscorecards.dev/projects/github.com/ortelius/scec-domain)
+
+![Discord](https://img.shields.io/discord/722468819091849316)
 
 ## Path Table
 
--- docs/docs.go
+++ docs/docs.go
@@ -87,11 +87,11 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "11.0.0",
-	Host:             "localhost:3000",
+	Host:             "localhost:8080",
 	BasePath:         "/msapi/domain",
 	Schemes:          []string{},
 	Title:            "Ortelius v11 Domain Microservice",
-	Description:      "RestAPI for the Domain Object",
+	Description:      "RestAPI for the Domain Object\n![Release](https://img.shields.io/github/v/release/ortelius/scec-domain?sort=semver)\n![license](https://img.shields.io/github/license/ortelius/scec-domain)\n\n![Build](https://img.shields.io/github/actions/workflow/status/ortelius/scec-domain/build-push-chart.yml)\n[![MegaLinter](https://github.com/ortelius/scec-domain/workflows/MegaLinter/badge.svg?branch=main)](https://github.com/ortelius/scec-domain/actions?query=workflow%3AMegaLinter+branch%3Amain)\n![CodeQL](https://github.com/ortelius/scec-domain/workflows/CodeQL/badge.svg)\n[![OpenSSF-Scorecard](https://api.securityscorecards.dev/projects/github.com/ortelius/scec-domain/badge)](https://api.securityscorecards.dev/projects/github.com/ortelius/scec-domain)\n\n![Discord](https://img.shields.io/discord/722468819091849316)",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
